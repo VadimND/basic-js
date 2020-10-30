0970849b6e2923e3a7b936d9b512eb2970139e7c
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-10-30 15:18:54 +0300

    feat: dream team and count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {  
+  let number = 0;
+  let str;
+  if(backyard.length === 0) {
+    number = 0;
+  }
+  str = backyard.join(',').replace(/ \^\^/gi, '').replace(/\^ \^/gi, '').match(/\^\^/g);
+  if (str !== null) {
+    number = str.length;
+  } else {
+    number = 0;
+  } 
+  return number;
+  //throw new CustomError('Not implemented');
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let name; 
+  let newArr = [];
+
+  if (Array.isArray(members)) {
+    for(let i = 0; i < members.length; i++) {      
+      if (typeof members[i] === 'string') {
+        members[i] = members[i].trim();
+        newArr.push((members[i][0]).toUpperCase());        
+      }
+    }
+  } else {
+    name = false;
+  }
+  name = newArr.sort().join('');
+  return name;
 };
